81de339d3a8698c159cbc1d418e53328601ad354
bednayb <bednaybence@gmail.com>
2016-12-08 12:15:25 +0100

    exercises

---
+++ week-07/day-04/classlist-1.html/classlist-1.js
@@ -0,0 +1,7 @@
+
+  // Does the third p have a cat class?
+  // If so, alert 'YEAH CAT!'
+  // If the fourth p has a 'dolphin' class, replace apple's content with 'pear'
+  // If the first p has an 'apple' class, replace cat's content with 'dog'
+  // Make apple red
+  // Make balloon less balloon-like
